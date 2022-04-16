f0e821445f74e8f4d0d24e9da78d35b3f6be6ef7
Farid <Farid.Masoum@snapp.market>
2022-04-16 16:16:47 +0430

    4nd

--- goimagefuns.go
+++ goimagefuns.go
@@ -1,11 +1,5 @@
 package goimagefuns
 
-import (
-	"faridmasoum/goimage/const/applicationConstants"
-	"faridmasoum/goimage/const/imageConstants"
-	"faridmasoum/goimage/const/menuCategoryConstants"
-)
-
 //func GetProductVariationImage(imageStruct ImageStruct) string  {
 //	// TODO:
 //	return nil
@@ -13,25 +7,25 @@ import (
 
 func getProductVariationThumbnailImage(imageStruct ImageStruct) string {
 	if imageStruct.FileName != nil {
-		imagePath := imageConstants.IMAGE_PATH
-		filterType := imageConstants.APP_IMAGE_THUMBNAIL_TYPE
+		imagePath := IMAGE_PATH
+		filterType := APP_IMAGE_THUMBNAIL_TYPE
 
-		if *imageStruct.UserType == imageConstants.USER_TYPE_ZOODFOOD {
+		if *imageStruct.UserType == USER_TYPE_ZOODFOOD {
 			if imageStruct.FileName == imageStruct.ImageName {
-				imagePath = menuCategoryConstants.PRODUCT_VARIATION_IMAGE_PATH
+				imagePath = PRODUCT_VARIATION_IMAGE_PATH
 			} else {
-				imagePath = menuCategoryConstants.USER_APP_IMAGE_PATH
+				imagePath = USER_APP_IMAGE_PATH
 			}
 
-			filterType = menuCategoryConstants.PRODUCT_VARIATION_APP_THUMBNAIL_IMAGE_TYPE
+			filterType = PRODUCT_VARIATION_APP_THUMBNAIL_IMAGE_TYPE
 		}
 
 		path := imagePath + *imageStruct.FileName
 
-		srcPath := applicationConstants.SNAPPMARKET_API_CDN_BASE + "/media/cache/" + filterType + path
+		srcPath := SNAPPMARKET_API_CDN_BASE + "/media/cache/" + filterType + path
 
 		return srcPath
 	} else {
-		return "";
+		return ""
 	}
 }
